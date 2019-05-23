6e8ec1e06388c992d8d8c8e44e41b62d6d9e1ffc
lanjunmm <1137262895@qq.com>
2019-05-23 12:47:36 +0800

    a simple server to test

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http').Server(app);
+
+const port = 3000;
+
+let Socket=null;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("*", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next()
+    }
+});
+app.get('/tranfer', function(req, res) {
+    console.log('get:');
+    res.send('.');
+});
+
+app.post('/trans', function(req, res) {
+    console.log('post:');
+    req.on("data",(data)=>{
+        // Socket.emit("message","mock response")
+        res.send('xhr response');
+    })
+});
+
+http.listen(port, function() {
+    console.log(`listening on port:${port}`);
+});
+
+const io = require('socket.io')(http,{transports:['polling','websocket']});//'polling'
+
+io.on('connection', function(socket) {
+    Socket=socket;
+    console.log('a user connected');
+    socket.on('message', function(data) {
+        console.log('Client-message: ',data);
+    })
+    socket.on('network', function(data) {
+        console.log('Client-network: ',data.type || data);
+        if(!!data.type && data.type==="network"){
+            socket.emit('network', {id:data.reqId} || "a msg from server");
+        }else{
+            socket.emit('network', data.type || "a msg from server");
+        }
+    })
+})
