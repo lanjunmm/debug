ac671a880f792d41c51129cab67251f6e1fbc3af
lanjunmm <1137262895@qq.com>
2019-06-03 17:21:05 +0800

    http请求，jsonp交互

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const http = require('http').Server(app);
 
 const port = 3000;
 
-let Socket=null;
+let workerSocket=null;
+let renderSocket=null;
+let ClientMap = new Map();
+let ReqMap = new Map();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("*", function (req, res, next) {
@@ -18,18 +21,52 @@ app.use("*", function (req, res, next) {
         next()
     }
 });
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+app.get('/testifr', function(req, res) {
+    res.render('testIframe.html');
+});
+app.get('/testIfrReq', function(req, res) {
+    res.send("iframeRes");
+});
 app.get('/tranfer', function(req, res) {
     console.log('get:');
     res.send('.');
 });
 
-app.post('/network', function(req, res) {
-    console.log('post');
+app.post('/fetch',function (req,res) {
+    console.log('fetch');
     req.on("data",(data)=>{
-        // Socket.emit("message","mock response")
-        res.send('xhr response');
+        data = JSON.parse(data);
+        ReqMap.set(data.reqId,res);
+        if(renderSocket!=null){
+            renderSocket.emit("fetch",data);
+        }
     })
-});
+})
+app.post('/xhr',function (req,res) {
+    console.log('xhr');
+    req.on("data",(data)=>{
+        data = JSON.parse(data);
+        ReqMap.set(data.reqId,res);
+        if(renderSocket!=null){
+            renderSocket.emit("xhr",data);
+        }
+    })
+})
+app.post('/beacon',function (req,res) {
+    console.log('beacon');
+    req.on("data",(data)=>{
+        data = JSON.parse(data);
+        ReqMap.set(data.reqId,res);
+        if(renderSocket!=null){
+            renderSocket.emit("beacon",data);
+        }
+    })
+})
 
 http.listen(port, function() {
     console.log(`listening on port:${port}`);
@@ -38,27 +75,71 @@ http.listen(port, function() {
 const io = require('socket.io')(http,{transports:['polling','websocket']});//'polling'
 
 io.on('connection', function(socket) {
-    Socket=socket;
     console.log('a user connected');
-    socket.emit("message","测试Emit");
-    socket.on('message', function(data) {
+    ClientMap.set(socket.id,socket);
+    socket.emit("id",socket.id);
+
+    socket.on("id",function (data) {
+        if(data.name==="worker"){
+            workerMsg(ClientMap.get(data.id));
+        }else if(data.name==="render"){
+            renderMsg(ClientMap.get(data.id));
+        }
+    });
+});
+
+function workerMsg(socket) {
+    workerSocket=socket;
+    workerSocket.on('message', function(data) {
         console.log('Client-message: ',data);
     });
-    socket.on('jsonp', function(data) {
-        socket.emit('jsonp', data.type || "a jsonp response msg from server");
+    workerSocket.on('jsonp', function(data) {
+        renderSocket.emit('jsonp',data);
     });
-    socket.on('event', function(data) {
-        socket.emit('event', data.type || "a event response msg from server");
+    workerSocket.on('event', function(data) {
+        workerSocket.emit('event', data.type || "a event response msg from server");
     });
-    socket.on('mutation', function(data) {
-        socket.emit('mutation', data.type || "a mutation response msg from server");
+    workerSocket.on('mutation', function(data) {
+        workerSocket.emit('mutation', data.type || "a mutation response msg from server");
     });
-    // socket.on('network', function(data) {
+    workerSocket.on('snapshot', function(data) {
+        workerSocket.emit('snapshot', data.type || "a snapshot response msg from server");
+    });
+    workerSocket.on('mouse', function(data) {
+        workerSocket.emit('mouse', data.type || "a mouse response msg from server");
+    });
+    // workerSocket.on('network', function(data) {
     //     console.log('Client-network: ',data.type || data);
     //     if(!!data.type && data.type==="network"){
-    //         socket.emit('network', {id:data.reqId} || "a msg from server");
+    //         workerSocket.emit('network', {id:data.reqId} || "a msg from server");
     //     }else{
-    //         socket.emit('network', data.type || "a msg from server");
+    //         workerSocket.emit('network', data.type || "a msg from server");
     //     }
     // })
-})
+}
+
+function httpRes(reseponseObj,resData) {
+    if(reseponseObj!=null){
+        reseponseObj.send(resData);
+        ReqMap.set(resData.reqId,null);
+    }
+}
+
+function renderMsg(socket) {
+    renderSocket = socket;
+    renderSocket.on("fetch",function (fetchRes) {
+        let reseponseObj = ReqMap.get(fetchRes.reqId);
+        httpRes(reseponseObj,fetchRes);
+    });
+    renderSocket.on("xhr",function (xhrRes) {
+        let reseponseObj = ReqMap.get(xhrRes.reqId);
+        httpRes(reseponseObj,xhrRes);
+    });
+    renderSocket.on("beacon",function (beaconRes) {
+        let reseponseObj = ReqMap.get(beaconRes.reqId);
+        httpRes(reseponseObj,beaconRes);
+    });
+    renderSocket.on('jsonp',function (jsonpRes) {
+        workerSocket.emit('jsonp', jsonpRes);
+    });
+}
