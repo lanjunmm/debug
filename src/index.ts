25de6c0ec00016faeaae2459890dd0b01c4ad8a3
lanjunmm <1137262895@qq.com>
2019-06-20 13:42:24 +0800

    打包:npm run build,使用:script引入www/static/js/app

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import socket from './utils/socket'
 export default class Render {
     constructor(){
         window['_debugSocket']=socket; // jsonp 全局script返回时
+        window['_sendToServer']=function sendToServer(funcName,data) {
+            let socket = window['_debugSocket'];
+            let msg = funcName+"("+JSON.stringify(data)+")";
+            console.log(msg);
+            socket.emit("jsonp",msg);
+        };
 
         const playerDefaultStyle = document.createElement('style');
         playerDefaultStyle.setAttribute('type', 'text/css');
@@ -10,4 +16,6 @@ export default class Render {
         document.head!.insertBefore(playerDefaultStyle, document.head!.firstChild!);
     }
 }
+/** 打包留出全局接口：*/
+window['render'] = new Render();
 
